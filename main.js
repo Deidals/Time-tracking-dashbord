e0ffebd60ac2609e7e134a5860c7a4ddeac8be8e
Alqorni Trikandalas Ginting <alqorniginting@gmail.com>
2024-12-09 16:08:00 +0700

    Ver 0.90

---
+++ main.js
@@ -0,0 +1,45 @@
+async function fetchAndUseData() {
+    try {
+      const response = await fetch('/data.json');
+      if (!response.ok) {
+        console.log('Oops! Something went wrong.');
+        return;
+      }
+      const data = await response.json();
+      
+      const dailyButton = document.querySelector(`#dailyButton`);
+      const weeklyButton = document.querySelector(`#weeklyButton`);
+      const monthlyButton = document.querySelector(`#monthlyButton`);
+
+      dailyButton.addEventListener(`click`, function(){
+        for (let i = 0; i < data.length; i++) {
+            // Your loop logic here
+            document.querySelector(`#card${i}`).innerHTML = `${data[i].timeframes.daily.current}hrs`;
+            document.querySelector(`#cardPrevious${i}`).innerHTML = `Last day - ${data[i].timeframes.daily.previous}hrs`;
+      }});
+
+      weeklyButton.addEventListener(`click`, function(){
+        for (let i = 0; i < data.length; i++) {
+            // Your loop logic here
+            document.querySelector(`#card${i}`).innerHTML = `${data[i].timeframes.weekly.current}hrs`;
+            document.querySelector(`#cardPrevious${i}`).innerHTML = `Last day - ${data[i].timeframes.weekly.previous}hrs`;
+      }});
+
+      monthlyButton.addEventListener(`click`, function(){
+        for (let i = 0; i < data.length; i++) {
+            // Your loop logic here
+            document.querySelector(`#card${i}`).innerHTML = `${data[i].timeframes.monthly.current}hrs`;
+            document.querySelector(`#cardPrevious${i}`).innerHTML = `Last day - ${data[i].timeframes.monthly.previous}hrs`;
+      }});
+
+      console.log(data);
+      console.log(data[0].timeframes.daily.current);
+      // Use the JSON data here
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  
+  fetchAndUseData();
+  
